f987ea39986eda7f9e9ac4d6844380afdb7ba4f7
Lucius47 <60123495+Lucius47@users.noreply.github.com>
2021-11-13 00:24:28 +0500

    Changes

---
+++ Assets/Scripts/AircraftPartsMotion.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AircraftPartsMotion : MonoBehaviour
+{
+    [SerializeField] Transform propeller;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propeller.Rotate(Vector3.up * 10000 * Time.deltaTime);
+    }
+}
